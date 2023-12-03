ceb4f24f06c5ff85751fbc2db706ef8dcd999108
Joseph.Sektak <39785561+pumsuankhaiSEKTAK@users.noreply.github.com>
2023-12-03 17:22:35 +1100

    Update sql_note.sql

--- sql_note.sql
+++ sql_note.sql
@@ -141,6 +141,27 @@ FROM
     employees e ON m.emp_no = e.emp_no;
 
 
+--e.g.,
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    t.title,
+    dm.from_date,
+    d.dept_name
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no
+        JOIN
+    departments d ON dm.dept_no = d.dept_no
+        JOIN
+    titles t ON e.emp_no = t.emp_no
+WHERE
+    t.title = 'Manager'
+ORDER BY e.emp_no;
+
+
 
 
 
