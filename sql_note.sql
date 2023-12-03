83758c6e9505026545067586bd00cfb327832ca2
Joseph.Sektak <39785561+pumsuankhaiSEKTAK@users.noreply.github.com>
2023-12-03 15:36:42 +1100

    Update sql_note.sql

--- sql_note.sql
+++ sql_note.sql
@@ -18,3 +18,28 @@ set @@global.sql_mode := replace(@@global.sql_mode, 'ONLY_FULL_GROUP_BY', '');
 
 -- Left JOIN or LEFT OUTER JOIN
 --- left joins can deliver a list with all records from the left table that do not match any rows from the right table
+e.g.2,
+SELECT 
+    m.dept_no, m.emp_no, d.dept_name
+FROM
+    dept_manager_dup m
+        LEFT JOIN
+    departments_dup d ON m.dept_no = d.dept_no
+WHERE
+    dept_name IS NOT NULL
+ORDER BY dept_no;
+
+--e.g.2,
+SELECT 
+    e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    d.dept_no, 
+    d.from_date
+FROM
+    employees e
+        LEFT JOIN
+    dept_manager d ON e.emp_no = d.emp_no
+WHERE
+    e.last_name = 'Markovitch'
+ORDER BY d.dept_no DESC, e.emp_no;
