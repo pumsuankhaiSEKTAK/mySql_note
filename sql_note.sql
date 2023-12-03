8b7e32142a77969e5e5e4b50fbe6e0b2809c2569
Joseph.Sektak <39785561+pumsuankhaiSEKTAK@users.noreply.github.com>
2023-12-03 16:31:47 +1100

    Update sql_note.sql

--- sql_note.sql
+++ sql_note.sql
@@ -101,5 +101,19 @@ WHERE
 ORDER BY dm.emp_no , d.dept_no;
 
 
+e.g., 
+SELECT 
+    e.*, d.*
+FROM
+    departments d
+        CROSS JOIN
+    dept_manager dm
+    JOIN
+    employees e ON dm.emp_no = e.emp_no
+WHERE
+    d.dept_no <> dm.dept_no
+ORDER BY dm.emp_no , d.dept_no;
+
+
 
 
