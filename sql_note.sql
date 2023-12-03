efb882e8289f33003c37d311911862fbf5727dfa
Joseph.Sektak <39785561+pumsuankhaiSEKTAK@users.noreply.github.com>
2023-12-03 19:12:13 +1100

    Update sql_note.sql

--- sql_note.sql
+++ sql_note.sql
@@ -223,6 +223,51 @@ select e.*
 from employees e 
 LIMIT 20;
 
+---Union example
+SELECT 
+e.emp_no,
+e.first_name,
+e.last_name,
+NULL AS dept_no,
+NULL AS from_date
+from
+employees_dup e
+Where 
+e.emp_no = 10001
+UNION SELECT
+NULL AS emp_no,
+NULL AS first_name,
+NULL AS last_name,
+m.dept_no,
+m.from_date
+From
+dept_manager m; 
+
+---UNION vs UNION ALL
+-- UNIONs displays only distinct values in the output
+-- UNION ALL retrieves the duplicates as well
+SELECT 
+    *
+FROM
+    (SELECT 
+        e.emp_no,
+            e.first_name,
+            e.last_name,
+            NULL AS dept_no,
+            NULL AS from_date
+    FROM
+        employees e
+    WHERE
+        last_name = 'Denis' UNION SELECT 
+        NULL AS emp_no,
+            NULL AS first_name,
+            NULL AS last_name,
+            dm.dept_no,
+            dm.from_date
+    FROM
+        dept_manager dm) AS a
+ORDER BY - a.emp_no DESC;
+
 
 
 
