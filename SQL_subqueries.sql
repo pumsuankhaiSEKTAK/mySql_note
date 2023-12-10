ba357732b80006183859f138a4afdd42ad8fa154
Joseph.Sektak <39785561+pumsuankhaiSEKTAK@users.noreply.github.com>
2023-12-10 13:00:57 +1100

    Update SQL_subqueries.sql

--- SQL_subqueries.sql
+++ SQL_subqueries.sql
@@ -1,1 +1,25 @@
+-- find firstname and lastname from employee table who are manager in dept_manager table
+SELECT 
+    e.emp_no, e.first_name, e.last_name
+FROM
+    employees e
+WHERE
+    e.emp_no IN (SELECT 
+            dm.emp_no
+        FROM
+            dept_manager dm);
 
+
+--exercise
+--extract the information about all department managers who were hired between the 1st of janary 1990 and the 1st january 1995.
+SELECT 
+    *
+FROM
+    dept_manager
+WHERE
+    emp_no IN (SELECT 
+            emp_no
+        FROM
+            employees
+        WHERE
+            hire_date BETWEEN '1990-01-01' AND '1995-01-01');
