df7d4b57fffeabc041a6f007ff654547f1bb4970
Joseph.Sektak <39785561+pumsuankhaiSEKTAK@users.noreply.github.com>
2023-12-10 15:36:57 +1100

    Update SQL_subqueries.sql

--- SQL_subqueries.sql
+++ SQL_subqueries.sql
@@ -23,3 +23,17 @@ WHERE
             employees
         WHERE
             hire_date BETWEEN '1990-01-01' AND '1995-01-01');
+
+
+-- Using EXISTS
+SELECT 
+    e.first_name, e.last_name
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            dept_manager dm
+        WHERE
+            dm.emp_no = e.emp_no);
